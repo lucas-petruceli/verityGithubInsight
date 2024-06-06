5f2398884287e5fffc73fab8baca6f8ddec0cd8f
Lucas petruceli Mattos <lucaspmtt@gmail.com>
2024-06-05 21:23:25 -0300

    criacao dos modulos

---
+++ settings.gradle.kts
@@ -0,0 +1,26 @@
+pluginManagement {
+    repositories {
+        google {
+            content {
+                includeGroupByRegex("com\\.android.*")
+                includeGroupByRegex("com\\.google.*")
+                includeGroupByRegex("androidx.*")
+            }
+        }
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "VerityGitHubInsight"
+include(":app")
+include(":feature:home")
+include(":feature:user")
+include(":core:network")
